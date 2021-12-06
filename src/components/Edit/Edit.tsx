efad6ee2e0b949b0105f7dfc9338491a26fe0e5c
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-12-06 07:46:03 +0330

    Handle edit of unexisting items

--- src/components/Edit/Edit.tsx
+++ src/components/Edit/Edit.tsx
@@ -1,11 +1,25 @@
 import { useQuery, useMutation, useQueryClient } from "react-query";
-import { useParams, useNavigate } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 import { getItem, editItem } from "../../api";
 import { IItem } from "../Items/Items";
 import Form from "../Form/Form";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    link: {
+      padding: "0 1rem",
+      color: "#1976D2",
+      textDecoration: "none",
+      fontSize: "1rem",
+    },
+  })
+);
+
 const Edit = () => {
+  const classes = useStyles();
+
   const { id } = useParams();
 
   const navigate = useNavigate();
@@ -24,7 +38,11 @@ const Edit = () => {
     navigate("/");
   };
 
-  return (
+  return data === "Not found" ? (
+    <Link className={classes.link} to="/">
+      Item not found. Go back...
+    </Link>
+  ) : (
     <Form defaultValues={data} onSubmit={onSubmit} isLoading={isLoading} />
   );
 };
