6a6b61b5af33887f95f0f86b9c9e027ad6921e26
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-12-09 22:02:58 +0330

    Bugfix for styles

--- src/components/Story/styles.ts
+++ src/components/Story/styles.ts
@@ -3,8 +3,7 @@ import { Styles } from "../../types";
 export const styles: Styles = {
   story: {
     display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
+    flexDirection: "column",
     width: "100%",
   },
   loader: {
