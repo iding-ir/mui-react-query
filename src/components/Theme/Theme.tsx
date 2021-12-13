a5383fc6a5e8b68da21aea6cb157956a7a1fb98e
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-12-13 16:15:23 +0330

    Customize theme switch

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -3,6 +3,7 @@ import Switch from "@mui/material/Switch";
 import { useTranslation } from "react-i18next";
 
 import { ThemeContext } from "./useTheme";
+import { styles } from "./styles";
 
 const Theme = () => {
   const { t } = useTranslation();
@@ -13,6 +14,7 @@ const Theme = () => {
     <Switch
       inputProps={{ "aria-label": t("Navbar.theme") }}
       checked={mode === "dark"}
+      sx={styles.switch}
       onChange={(event: ChangeEvent<HTMLInputElement>) => {
         setMode(event.target.checked ? "dark" : "light");
       }}
